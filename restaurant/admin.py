b47146d42d4292cfcf2d2fa8f69b232d9f0d756b
a-rod85 <31aaronanderson@gmail.com>
2022-12-13 10:05:18 +0000

    add to admin.py, register restaurant, table, seat, reservation, menu

---
+++ restaurant/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Restaurant, Table, Seat, Reservation, Menu
+
+admin.site.register(Restaurant)
+admin.site.register(Table)
+admin.site.register(Seat)
+admin.site.register(Reservation)
+admin.site.register(Menu)
+
+
+# Register your models here.
